1c5945f9373ed86bfa1cf110caa125c7bc9508ae
Boleyn Su <boleyn.su@gmail.com>
2016-10-12 23:01:50 +0800

    update example

--- example/q.pl
+++ example/q.pl
@@ -1,3 +1,11 @@
 willDie(a).
 willDie(b).
 
+concat(cons(a,cons(b,nil)),cons(c,nil),X).
+
+add(s(s(z)),X,s(s(s(z)))).
+
+mul(s(s(z)),X,s(s(s(s(s(s(z))))))).
+
+mul(X,Y,s(s(s(s(s(s(z))))))).
+
--- example/r.pl
+++ example/r.pl
@@ -1,3 +1,12 @@
 willDie(X):-isHuman(X).
 isHuman(a).
 
+concat(nil,Y,Y).
+concat(cons(X,XS),Y,cons(X,Z)):-concat(XS,Y,Z).
+
+add(z,Y,Y).
+add(s(X),Y,s(Z)):-add(X,Y,Z).
+
+mul(z,Y,z).
+mul(s(X),Y,Z):-add(T,Y,Z),mul(X,Y,T).
+
